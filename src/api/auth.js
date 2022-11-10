7ba7f6cc4152ea3e9ffc1e6c268f70bcfbfaefa2
nafisamou <108416863+nafisamou@users.noreply.github.com>
2022-11-10 21:22:41 +0600

    loading spinner updated

--- src/api/auth.js
+++ src/api/auth.js
@@ -14,7 +14,7 @@ export const setAuthToken = (user) => {
     .then((res) => res.json())
     .then((data) => {
       console.log(data);
-      // local storage is easiest but not the best place to jwt store token  ssssssssssss.
+      // local storage is easiest but not the best place to jwt store token  sssssssssssssss.
       localStorage.setItem("kitchen-token", data.token);
     });
 };
